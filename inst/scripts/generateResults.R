5d15b59fa566d43036389c0b08ce0f2ea6b95345
Emma Davis <61745476+emmalouisedavis@users.noreply.github.com>
2021-05-12 16:53:04 +0100

    Update generateResults.R

--- inst/scripts/generateResults.R
+++ inst/scripts/generateResults.R
@@ -79,7 +79,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results1, file = "data-raw/res_Jan_1_fix.rds")
+saveRDS(sweep_results1, file = "data-raw/res_1.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -148,7 +148,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results2, file = "data-raw/res_Jan_2_fix.rds")
+saveRDS(sweep_results2, file = "data-raw/res_2.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -217,7 +217,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results2b, file = "data-raw/res_Jan_2b_fix.rds")
+saveRDS(sweep_results2b, file = "data-raw/res_2b.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -286,7 +286,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results3, file = "data-raw/res_Jan_3_fix.rds")
+saveRDS(sweep_results3, file = "data-raw/res_3.rds")
 rm(list=ls())
 
 ##################################################################
@@ -353,7 +353,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results4, file = "data-raw/res_Jan_4_fix.rds")
+saveRDS(sweep_results4, file = "data-raw/res_4.rds")
 rm(list=ls())
 
 ##################################################################
@@ -421,7 +421,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results5, file = "data-raw/res_Jan_5_fix.rds")
+saveRDS(sweep_results5, file = "data-raw/res_5.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -489,7 +489,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results5b, file = "data-raw/res_Jan_5b_fix.rds")
+saveRDS(sweep_results5b, file = "data-raw/res_5b.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -556,7 +556,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results6, file = "data-raw/res_Jan_6_fix.rds")
+saveRDS(sweep_results6, file = "data-raw/res_6.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -625,7 +625,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results7, file = "data-raw/res_Jan_7_fix.rds")
+saveRDS(sweep_results7, file = "data-raw/res_7.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -693,7 +693,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results8, file = "data-raw/res_Jan_8_fix.rds")
+saveRDS(sweep_results8, file = "data-raw/res_8.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -761,7 +761,7 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results8b, file = "data-raw/res_Jan_8b_fix.rds")
+saveRDS(sweep_results8b, file = "data-raw/res_8b.rds")
 rm(list=ls())
 Sys.sleep(120)
 
@@ -829,25 +829,25 @@ toc()
 
 
 # #+ writeout
-saveRDS(sweep_results9, file = "data-raw/res_Jan_9_fix.rds")
+saveRDS(sweep_results9, file = "data-raw/res_9.rds")
 rm(list=ls())
 Sys.sleep(120)
 
   ##################################################################
 
 # Load in separate sets of scenarios and combine
-sweep_results1 <- readRDS("data-raw/res_Jan_1_fix.rds")
-sweep_results2 <- readRDS("data-raw/res_Jan_2_fix.rds")
-sweep_results2b <- readRDS("data-raw/res_Jan_2b_fix.rds")
-sweep_results3 <- readRDS("data-raw/res_Jan_3_fix.rds")
-sweep_results4 <- readRDS("data-raw/res_Jan_4_fix.rds")
-sweep_results5 <- readRDS("data-raw/res_Jan_5_fix.rds")
-sweep_results5b <- readRDS("data-raw/res_Jan_5b_fix.rds")
-sweep_results6 <- readRDS("data-raw/res_Jan_6_fix.rds")
-sweep_results7 <- readRDS("data-raw/res_Jan_7_fix.rds")
-sweep_results8 <- readRDS("data-raw/res_Jan_8_fix.rds")
-sweep_results8b <- readRDS("data-raw/res_Jan_8b_fix.rds")
-sweep_results9 <- readRDS("data-raw/res_Jan_9_fix.rds")
+sweep_results1 <- readRDS("data-raw/res_1.rds")
+sweep_results2 <- readRDS("data-raw/res_2.rds")
+sweep_results2b <- readRDS("data-raw/res_2b.rds")
+sweep_results3 <- readRDS("data-raw/res_3.rds")
+sweep_results4 <- readRDS("data-raw/res_4.rds")
+sweep_results5 <- readRDS("data-raw/res_5.rds")
+sweep_results5b <- readRDS("data-raw/res_5b.rds")
+sweep_results6 <- readRDS("data-raw/res_6.rds")
+sweep_results7 <- readRDS("data-raw/res_7.rds")
+sweep_results8 <- readRDS("data-raw/res_8.rds")
+sweep_results8b <- readRDS("data-raw/res_8b.rds")
+sweep_results9 <- readRDS("data-raw/res_9.rds")
 #
 
 sweep_results <- as_tibble(rbind(sweep_results1,sweep_results2,sweep_results2b,sweep_results3,sweep_results4,sweep_results5,
@@ -875,216 +875,5 @@ cap_cases <- 2000
 max_days <- 300
 
 # # # Save final combined results (DONE)
-saveRDS(sweep_results, file = "data-raw/res_Jan_complete_fix.rds")
-
-
-#########################
-# Missed Chains (Fig 3a and b):
-
-# 5 initial cases
-
-rm(list = ls())
-devtools::load_all()
-
-set.seed(200518)
-no.samples <- 5000
-seed.cases <- 5
-
-scenarios <- tidyr::expand_grid(
-  ## Put parameters that are grouped by disease into this data.frame
-  delay_group = list(tibble::tibble(
-    delay = c("Adherence"),
-    delay_shape = c(0.06), # probability of self-isolation if symptomatic, assume == hospitalisation proportion
-    delay_scale = 5.95402 # time from onset to self-isolation (i.e. hospitalisation)
-  )),
-  inc_meanlog = 1.434065,
-  inc_sdlog = 0.6612,
-  inf_shape = 17.773185,
-  inf_rate = 1.388388,
-  inf_shift = 12.978985,
-  min_quar_delay = 1,
-  max_quar_delay = c(1),
-  index_R0 = c(1.1,1.3,1.5),
-  prop.asym = c(0.4),
-  control_effectiveness = 1,
-  #self_report = proportion of self-isolating cases that self-report into tracing system
-  # (assume only hospitalised cases "isolate"/detected, i.e. all isolating cases are reported)
-  self_report = c(1),
-  test_delay = c(0), #time from isolation to test result
-  sensitivity = c(0), #sensitivity of test
-  precaution = c(0), #this could be between 0 and 7? Number of days stay in isolation if negative test
-  num.initial.cases = seed.cases) %>%
-  tidyr::unnest("delay_group") %>%
-  dplyr::mutate(scenario = 1:dplyr::n())
-
-cap_cases <- 2000
-max_days <- 50
-## Parameterise fixed paramters
-sim_with_params <- purrr::partial(ringbp::scenario_sim,
-                                  cap_max_days = max_days,
-                                  cap_cases = cap_cases,
-                                  r0isolated = 0,
-                                  disp.iso = 1,
-                                  disp.com = 0.16,
-                                  quarantine = TRUE)
-
-#+ full_run
-tic()
-## Run parameter sweep
-sweep_results <- ringbp::parameter_sweep(scenarios,
-                                         sim_fn = sim_with_params,
-                                         samples = no.samples,
-                                         show_progress = TRUE,
-                                         earlyOut = TRUE)
-toc()
-
-sweep_results$sims[[1]] <- sweep_results$sims[[1]] %>% group_by(sim) %>%
-  mutate(early_missed = c(unique(early_missed),rep(NA,7))) %>%
-  mutate(first_iso = c(unique(first_iso),rep(NA,7))) %>%
-  mutate(index_R0 = paste(sweep_results$index_R0[1])) %>%
-  ungroup()
-sweep_results$sims[[2]] <- sweep_results$sims[[2]] %>% group_by(sim) %>%
-  mutate(early_missed = c(unique(early_missed),rep(NA,7))) %>%
-  mutate(first_iso = c(unique(first_iso),rep(NA,7))) %>%
-  mutate(index_R0 = paste(sweep_results$index_R0[2])) %>%
-  ungroup()
-sweep_results$sims[[3]] <- sweep_results$sims[[3]] %>% group_by(sim) %>%
-  mutate(early_missed = c(unique(early_missed),rep(NA,7))) %>%
-  mutate(first_iso = c(unique(first_iso),rep(NA,7))) %>%
-  mutate(index_R0 = paste(sweep_results$index_R0[3])) %>%
-  ungroup()
-
-saveRDS(sweep_results, file = "data-raw/res_Aug_missedChains_5cases.rds")
-
-# 100 initial cases
-
-rm(list = ls())
-devtools::load_all()
-
-set.seed(200518)
-no.samples <- 5000
-seed.cases <- 100
-
-scenarios <- tidyr::expand_grid(
-  ## Put parameters that are grouped by disease into this data.frame
-  delay_group = list(tibble::tibble(
-    delay = c("Adherence"),
-    delay_shape = c(0.06), # probability of self-isolation if symptomatic, assume == hospitalisation proportion
-    delay_scale = 5.95402 # time from onset to self-isolation (i.e. hospitalisation)
-  )),
-  inc_meanlog = 1.434065,
-  inc_sdlog = 0.6612,
-  inf_shape = 17.773185,
-  inf_rate = 1.388388,
-  inf_shift = 12.978985,
-  min_quar_delay = 1,
-  max_quar_delay = c(1),
-  index_R0 = c(1.1,1.3,1.5),
-  prop.asym = c(0.4),
-  control_effectiveness = 1,
-  #self_report = proportion of self-isolating cases that self-report into tracing system
-  # (assume only hospitalised cases "isolate"/detected, i.e. all isolating cases are reported)
-  self_report = c(1),
-  test_delay = c(0), #time from isolation to test result
-  sensitivity = c(0), #sensitivity of test
-  precaution = c(0), #this could be between 0 and 7? Number of days stay in isolation if negative test
-  num.initial.cases = seed.cases) %>%
-  tidyr::unnest("delay_group") %>%
-  dplyr::mutate(scenario = 1:dplyr::n())
-
-cap_cases <- 2000
-max_days <- 50
-## Parameterise fixed paramters
-sim_with_params <- purrr::partial(ringbp::scenario_sim,
-                                  cap_max_days = max_days,
-                                  cap_cases = cap_cases,
-                                  r0isolated = 0,
-                                  disp.iso = 1,
-                                  disp.com = 0.16,
-                                  quarantine = TRUE)
-
-#+ full_run
-tic()
-## Run parameter sweep
-sweep_results <- ringbp::parameter_sweep(scenarios,
-                                         sim_fn = sim_with_params,
-                                         samples = no.samples,
-                                         show_progress = TRUE,
-                                         earlyOut = TRUE)
-toc()
-
-sweep_results$sims[[1]] <- sweep_results$sims[[1]] %>% group_by(sim) %>%
-  mutate(early_missed = c(unique(early_missed),rep(NA,7))) %>%
-  mutate(first_iso = c(unique(first_iso),rep(NA,7))) %>%
-  mutate(index_R0 = paste(sweep_results$index_R0[1])) %>%
-  ungroup()
-sweep_results$sims[[2]] <- sweep_results$sims[[2]] %>% group_by(sim) %>%
-  mutate(early_missed = c(unique(early_missed),rep(NA,7))) %>%
-  mutate(first_iso = c(unique(first_iso),rep(NA,7))) %>%
-  mutate(index_R0 = paste(sweep_results$index_R0[2])) %>%
-  ungroup()
-sweep_results$sims[[3]] <- sweep_results$sims[[3]] %>% group_by(sim) %>%
-  mutate(early_missed = c(unique(early_missed),rep(NA,7))) %>%
-  mutate(first_iso = c(unique(first_iso),rep(NA,7))) %>%
-  mutate(index_R0 = paste(sweep_results$index_R0[3])) %>%
-  ungroup()
-
-saveRDS(sweep_results, file = "data-raw/res_Aug_missedChains_100cases.rds")
-
-#########################
-# Perfect Tracing (Fig 2)
-
-rm(list = ls())
-devtools::load_all()
-
-set.seed(200518)
-no.samples <- 5000
-
-scenarios <- tidyr::expand_grid(
-  ## Put parameters that are grouped by disease into this data.frame
-  delay_group = list(tibble::tibble(
-    delay = c("Adherence"),
-    delay_shape = c(0.9),
-    delay_scale = 1
-  )),
-  inc_meanlog = 1.434065,
-  inc_sdlog = 0.6612,
-  inf_shape = 17.773185,
-  inf_rate = 1.388388,
-  inf_shift = 12.978985,
-  min_quar_delay = 1,
-  max_quar_delay = c(1),
-  index_R0 = c(1.1,1.3,1.5),
-  prop.asym = c(0.4),
-  control_effectiveness = 1,
-  self_report = c(0.5,1),
-  test_delay = c(2), #time from isolation to test result
-  sensitivity = c(0.65,0.95), #percent of cases detected
-  precaution = c(7), #this could be between 0 and 7? Number of days stay in isolation if negative test
-  num.initial.cases = c(5)) %>%
-  tidyr::unnest("delay_group") %>%
-  dplyr::mutate(scenario = 1:dplyr::n())
-
-cap_cases <- 2000
-max_days <- 300
-## Parameterise fixed paramters
-sim_with_params <- purrr::partial(ringbp::scenario_sim,
-                                  cap_max_days = max_days,
-                                  cap_cases = cap_cases,
-                                  r0isolated = 0,
-                                  disp.iso = 1,
-                                  disp.com = 0.16,
-                                  quarantine = TRUE)
-
-#+ full_run
-tic()
-## Run parameter sweep
-sweep_results <- ringbp::parameter_sweep(scenarios,
-                                         sim_fn = sim_with_params,
-                                         samples = no.samples,
-                                         show_progress = TRUE,
-                                         earlyOut = FALSE)
-toc()
-
-saveRDS(sweep_results, file = "data-raw/res_Aug_perfectTracing.rds")
+saveRDS(sweep_results, file = "data-raw/res_complete.rds")
 
